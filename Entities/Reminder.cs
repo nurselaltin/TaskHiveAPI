ca46afcef10d745e3f62c8bd7fa05a3fc41e3b05
nurselaltin <nurselaltin.na@gmail.com>
2024-08-14 07:11:43 +0300

    Added Task and Reminder model.

---
+++ Entities/Reminder.cs
@@ -0,0 +1,12 @@
+﻿namespace Entities
+{
+    public  class Reminder
+    {
+        public int Id { get; set; }
+        public Guid TaskId { get; set; }
+        public TimeSpan TimeBeforeDue { get; set; } //Görevin bitiş tarihinden kaç gün önceden bildirim göndermeye başlamalı
+
+        public Task Task { get; set; }
+
+    }
+}
---
+++ Entities/Task.cs
@@ -0,0 +1,31 @@
+﻿namespace Entities
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; } //Görev bitiş tarihi
+        public bool IsCompleted { get; set; }
+
+        public List<Reminder> Reminders { get; set; }
+
+        public Task()
+        {
+            IsCompleted = false;
+            Reminders = new List<Reminder>();
+        }
+
+        public void UpdateDetails(string title, string desc, DateTime dueDate)
+        {
+            this.Title = title;
+            this.Description = desc;
+            this.DueDate = dueDate;
+        }
+
+        public void AddReminder(Reminder reminder)
+        {
+            Reminders.Add(reminder);
+        }
+    }
+}
